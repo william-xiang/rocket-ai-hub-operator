5c0b0fa24a8e6985895327cb5bd149a22f5412d1
William Xiang <william.xiang1375@gmail.com>
2025-05-20 12:08:06 -0400

    Revert "change the crd to namespaced scope"
    This reverts commit 40a958ea5650a335e8f50b113657bdafeb26d780.

--- api/v1alpha1/rocketaihub_types.go
+++ api/v1alpha1/rocketaihub_types.go
@@ -40,7 +40,7 @@ type RocketAIHubStatus struct {
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
-//+kubebuilder:resource:scope=Namespaced
+//+kubebuilder:resource:scope=Cluster
 
 // RocketAIHub is the Schema for the rocketaihubs API
 type RocketAIHub struct {

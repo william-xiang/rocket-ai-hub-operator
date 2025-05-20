a9e2d3c58d12ebc4e4b6c8bd3d074adc24c15e94
William Xiang <william.xiang1375@gmail.com>
2025-05-20 12:24:48 -0400

    create the admission webhook for rocket ai hub type
    Signed-off-by: William Xiang <william.xiang1375@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -138,6 +138,12 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "RocketAIHub")
 		os.Exit(1)
 	}
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = (&operatorv1alpha1.RocketAIHub{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "RocketAIHub")
+			os.Exit(1)
+		}
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
